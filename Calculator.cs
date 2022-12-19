7d61c058be07e453feaec8ea02cd9bd2092d1696
RickySin11 <rajvir11singh@outlook.com>
2022-12-19 15:56:22 -0800

    Keep a cache of previous calculations and allow the user to view and clear the cache

--- Calculator.cs
+++ Calculator.cs
@@ -1,30 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
+
 internal class Calculator
 {
+    public static int TotalOperations { get; private set; } = 0;
+    private static List<Tuple<double, double, string?, double>> CachedOperations { get; set; } = 
+        new List<Tuple<double, double, string?, double>>();
     public static double DoOperation(double num1, double num2, string? op)
     {
+        IncrementOperations();
+
+        double result;
+
         switch (op)
         {
             case "+":
-                return num1 + num2;
+                result = num1 + num2;
+                break;
             case "-":
-                return num1 - num2;
+                result = num1 - num2;
+                break;
             case "*":
-                return num1 * num2;
+                result = num1 * num2;
+                break;
             case "/":
                 if (num2 == 0)
                 {
                     throw new DivideByZeroException("Second Argument can not be zero when dividing.");
                 }
-                return num1 / num2;
+                result = num1 / num2;
+                break;
             default:
-                return double.NaN;
+                result = double.NaN;
+                break;
         }
+
+        CacheOperation(num1, num2, op, result);
+        return result;
+
     }
 
+    private static void CacheOperation(double num1, double num2, string? op, double result)
+    {
+        CachedOperations.Add(Tuple.Create(num1, num2, op, result));
+    }
+
+    private static void IncrementOperations()
+    {
+        TotalOperations++;
+    }
+
+    public static string ViewCache()
+    {
+        var sb = new StringBuilder();
+        foreach(var op in CachedOperations)
+        {
+            sb.AppendLine($"{op.Item1} {op.Item3} {op.Item2} = {op.Item4}");
+        }
+        return sb.ToString();
+        
+    }
+
+    public static void ClearCache()
+    {
+        CachedOperations.Clear();
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,62 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-double num1, num2;
-
-// Display title as the c# console calculator app.
-Console.WriteLine("Console Calculator in c#\r");
-Console.WriteLine("-------------------------\n");
-
-bool isCalculatorRunning = true;
-while (isCalculatorRunning)
+﻿internal class Program
 {
-    // Ask the user to type the first number.
-    Console.WriteLine("Type a number, and then press Enter");
-    while (!double.TryParse(Console.ReadLine(), out num1))
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Invalid input. Must enter a number:");
-    }
+        double num1, num2;
 
-    // Ask the user to type the second number.
-    Console.WriteLine("Type another number, and then press Enter");
-    while (!double.TryParse(Console.ReadLine(), out num2))
-    {
-        Console.WriteLine("Invalid input. Must enter a number:");
-    }
+        // Display title as the c# console calculator app.
+        Console.WriteLine("Console Calculator in c#\r");
+        Console.WriteLine("-------------------------\n");
 
-    // Ask the user to choose an option.
-    Console.WriteLine("Choose an option from the following list:");
-    Console.WriteLine("\t+ - Add");
-    Console.WriteLine("\t- - Subtract");
-    Console.WriteLine("\t* - Multiple");
-    Console.WriteLine("\t/ - Divide");
-    Console.WriteLine("Your option?");
-
-    // Use a switch statement to do the math.
-    try
-    {
-        string? op = Console.ReadLine();
-        double result = Calculator.DoOperation(num1, num2, op);
-        if (double.IsNaN(result))
+        bool isCalculatorRunning = true;
+        while (isCalculatorRunning)
         {
-            Console.WriteLine("Invalid Operator.");
-        } else
-        {
-            Console.WriteLine($"{num1} {op} {num2} = {result}");
+            Console.WriteLine("Would you like to see previous calculations (y or n)?");
+            string? response = Console.ReadLine();
+            if (!string.IsNullOrEmpty(response) && response[0] == 'y')
+            {
+                string cache = Calculator.ViewCache();
+                Console.WriteLine("List of previous operations:");
+                Console.WriteLine(cache);
+            }
+
+            Console.WriteLine("Would you like to clear previous calculations (y or n)?");
+            response = Console.ReadLine();
+            if (!string.IsNullOrEmpty(response) && response[0] == 'y')
+            {
+                Calculator.ClearCache();
+                Console.WriteLine("Calculator has been cleared.");
+            }
+
+            // Ask the user to type the first number.
+            Console.WriteLine("Type a number, and then press Enter");
+            while (!double.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.WriteLine("Invalid input. Must enter a number:");
+            }
+
+            // Ask the user to type the second number.
+            Console.WriteLine("Type another number, and then press Enter");
+            while (!double.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.WriteLine("Invalid input. Must enter a number:");
+            }
+
+            // Ask the user to choose an option.
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\t+ - Add");
+            Console.WriteLine("\t- - Subtract");
+            Console.WriteLine("\t* - Multiple");
+            Console.WriteLine("\t/ - Divide");
+            Console.WriteLine("Your option?");
+
+            // Use a switch statement to do the math.
+            try
+            {
+                string? op = Console.ReadLine();
+                double result = Calculator.DoOperation(num1, num2, op);
+                if (double.IsNaN(result))
+                {
+                    Console.WriteLine("Invalid Operator.");
+                }
+                else
+                {
+                    Console.WriteLine($"{num1} {op} {num2} = {result}");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input.");
+            }
+
+            // See whether user would like to continue.
+            Console.WriteLine("Would you like to go again (y or n).");
+            response = Console.ReadLine();
+            isCalculatorRunning = string.IsNullOrEmpty(response) && response![0] == 'y';
+
+            Console.WriteLine();
         }
-    } catch (Exception)
-    {
-        Console.WriteLine("Invalid input.");
+
+        Console.WriteLine($"Total number of operations: {Calculator.TotalOperations}");
+
+
+        // Wait for the user to respond before closing.
+        Console.WriteLine("Press any key to close the Calculator console app...");
+        Console.ReadKey();
     }
-
-    // See whether user would like to continue.
-    Console.WriteLine("Would you like to go again (y or n).");
-    string? response = Console.ReadLine();
-    isCalculatorRunning = string.IsNullOrEmpty(response) && response![0] == 'y';
-
-    Console.WriteLine();
 }
-
-
-// Wait for the user to respond before closing.
-Console.WriteLine("Press any key to close the Calculator console app...");
-Console.ReadKey();
