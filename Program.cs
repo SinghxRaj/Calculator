b2b1cc1976dba49930d85769f0c84592ab692f93
RickySin11 <rajvir11singh@outlook.com>
2022-12-20 05:49:55 -0800

    Change logging to store calculator operations as a JSON object.

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ internal class Program
             continueCalculator = SingleCalculation(calculator);
             Console.WriteLine();
         }
-
+        calculator.Finish();
         DisplayTotalCalculations(calculator);
     }
 
