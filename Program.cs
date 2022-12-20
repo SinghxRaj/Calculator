5b62ef000005d936bf2f517a8a2cff4589395315
RickySin11 <rajvir11singh@outlook.com>
2022-12-19 16:47:15 -0800

    Refactor runner.

--- Program.cs
+++ Program.cs
@@ -2,86 +2,133 @@
 {
     private static void Main(string[] args)
     {
-        double num1, num2;
+        DisplayTitle();
+        RunCalculator();
+        DisplayExit();
+    }
 
-        // Display title as the c# console calculator app.
-        Console.WriteLine("Console Calculator in c#\r");
-        Console.WriteLine("-------------------------\n");
-
-        bool isCalculatorRunning = true;
-        while (isCalculatorRunning)
+    private static void RunCalculator()
+    {
+        bool continueCalculator = true;
+        while (continueCalculator)
         {
-            Console.WriteLine("Would you like to see previous calculations (y or n)?");
-            string? response = Console.ReadLine();
-            if (!string.IsNullOrEmpty(response) && response[0] == 'y')
-            {
-                string cache = Calculator.ViewCache();
-                Console.WriteLine("List of previous operations:");
-                Console.WriteLine(cache);
-            }
-
-            Console.WriteLine("Would you like to clear previous calculations (y or n)?");
-            response = Console.ReadLine();
-            if (!string.IsNullOrEmpty(response) && response[0] == 'y')
-            {
-                Calculator.ClearCache();
-                Console.WriteLine("Calculator has been cleared.");
-            }
-
-            // Ask the user to type the first number.
-            Console.WriteLine("Type a number, and then press Enter");
-            while (!double.TryParse(Console.ReadLine(), out num1))
-            {
-                Console.WriteLine("Invalid input. Must enter a number:");
-            }
-
-            // Ask the user to type the second number.
-            Console.WriteLine("Type another number, and then press Enter");
-            while (!double.TryParse(Console.ReadLine(), out num2))
-            {
-                Console.WriteLine("Invalid input. Must enter a number:");
-            }
-
-            // Ask the user to choose an option.
-            Console.WriteLine("Choose an option from the following list:");
-            Console.WriteLine("\t+ - Add");
-            Console.WriteLine("\t- - Subtract");
-            Console.WriteLine("\t* - Multiple");
-            Console.WriteLine("\t/ - Divide");
-            Console.WriteLine("Your option?");
-
-            // Use a switch statement to do the math.
-            try
-            {
-                string? op = Console.ReadLine();
-                double result = Calculator.DoOperation(num1, num2, op);
-                if (double.IsNaN(result))
-                {
-                    Console.WriteLine("Invalid Operator.");
-                }
-                else
-                {
-                    Console.WriteLine($"{num1} {op} {num2} = {result}");
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Invalid input.");
-            }
-
-            // See whether user would like to continue.
-            Console.WriteLine("Would you like to go again (y or n).");
-            response = Console.ReadLine();
-            isCalculatorRunning = string.IsNullOrEmpty(response) && response![0] == 'y';
-
+            continueCalculator = SingleCalculation();
             Console.WriteLine();
         }
 
-        Console.WriteLine($"Total number of operations: {Calculator.TotalOperations}");
+        DisplayTotalCalculations();
+    }
 
-
+    private static void DisplayExit()
+    {
         // Wait for the user to respond before closing.
         Console.WriteLine("Press any key to close the Calculator console app...");
         Console.ReadKey();
+    }
+
+    private static void DisplayTitle()
+    {
+        // Display title as the c# console calculator app.
+        Console.WriteLine("Console Calculator in c#\r");
+        Console.WriteLine("-------------------------\n");
+    }
+
+    private static bool SingleCalculation()
+    {
+        bool isCalculatorRunning;
+        AskToViewPreviousCalculations();
+        AskToClearCache();
+        PerformCalculation();
+        isCalculatorRunning = ContinueCalculator();
+
+        return isCalculatorRunning;
+    }
+
+    private static void DisplayTotalCalculations()
+    {
+        Console.WriteLine($"Total number of operations: {Calculator.TotalOperations}");
+    }
+
+    private static bool ContinueCalculator()
+    {
+        bool isCalculatorRunning;
+        // See whether user would like to continue.
+        Console.WriteLine("Would you like to go again (y or n).");
+        string? response = Console.ReadLine();
+        isCalculatorRunning = string.IsNullOrEmpty(response) && response![0] == 'y';
+        return isCalculatorRunning;
+    }
+
+    private static void PerformCalculation()
+    {
+        double num1, num2;
+        GetOperands(out num1, out num2);
+
+        // Ask the user to choose an option.
+        Console.WriteLine("Choose an option from the following list:");
+        Console.WriteLine("\t+ - Add");
+        Console.WriteLine("\t- - Subtract");
+        Console.WriteLine("\t* - Multiple");
+        Console.WriteLine("\t/ - Divide");
+        Console.WriteLine("Your option?");
+
+        // Use a switch statement to do the math.
+        try
+        {
+            string? op = Console.ReadLine();
+            double result = Calculator.DoOperation(num1, num2, op);
+            if (double.IsNaN(result))
+            {
+                Console.WriteLine("Invalid Operator.");
+            }
+            else
+            {
+                Console.WriteLine($"{num1} {op} {num2} = {result}");
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Invalid input.");
+        }
+    }
+
+    private static void GetOperands(out double num1, out double num2)
+    {
+        // Ask the user to type the first number.
+        Console.WriteLine("Type a number, and then press Enter");
+        while (!double.TryParse(Console.ReadLine(), out num1))
+        {
+            Console.WriteLine("Invalid input. Must enter a number:");
+        }
+
+        // Ask the user to type the second number.
+        Console.WriteLine("Type another number, and then press Enter");
+        while (!double.TryParse(Console.ReadLine(), out num2))
+        {
+            Console.WriteLine("Invalid input. Must enter a number:");
+        }
+    }
+
+    private static void AskToClearCache()
+    {
+        Console.WriteLine("Would you like to clear previous calculations (y or n)?");
+        string? response = Console.ReadLine();
+        if (!string.IsNullOrEmpty(response) && response[0] == 'y')
+        {
+            Calculator.ClearCache();
+            Console.WriteLine("Calculator has been cleared.");
+        }
+    }
+
+    private static void AskToViewPreviousCalculations()
+    {
+        Console.WriteLine("Would you like to see previous calculations (y or n)?");
+        string? response = Console.ReadLine();
+        if (!string.IsNullOrEmpty(response) && response[0] == 'y')
+        {
+            string cache = Calculator.ViewCache();
+            Console.WriteLine("List of previous operations:");
+            Console.WriteLine(cache);
+        }
     }
 }
